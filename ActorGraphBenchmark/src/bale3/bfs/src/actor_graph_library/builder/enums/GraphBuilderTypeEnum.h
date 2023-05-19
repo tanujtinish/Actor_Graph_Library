3e6685ded5775edf509a40d6862a5e5a0a650af9
Tanuj Gupta <tanujgupta@tanujs-air.lan>
2023-05-18 18:56:55 -0700

    Added enum for builder type to be passed to builder class

---
+++ ActorGraphBenchmark/src/bale3/bfs/src/actor_graph_library/builder/enums/GraphBuilderTypeEnum.h
@@ -0,0 +1,5 @@
+enum GraphBuilderTypeEnum {
+    Cyclic = 1,
+    Range = 2,
+    HashPartition = 3,
+};
---
+++ ActorGraphBenchmark/src/bale3/bfs/src/actor_graph_library/builder/enums/HashAlgorithmEnum.h
@@ -0,0 +1,6 @@
+enum HashAlgorithmEnum {
+    SNAKE_HASH = 1,
+    FIBONACCI_HASH = 2,
+    XOR_HASH = 3,
+    FIBONACCI_XOR_HASH = 4
+};
